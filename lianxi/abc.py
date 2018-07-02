a95f105897462a630d8312eabea6a0f905c3e3e1
徐国强 <xuguoqiang@xuguoqiangs-MacBook-Pro.local>
2018-07-02 22:43:28 +0800

    新

---
+++ lianxi/abc.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    # 定义表名
+    __tablename__ = 'roles'
+    # 定义列对象
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    us = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64),unique=True)
+    pswd = db.Column(db.String(64))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
---
+++ lianxi/config.py
@@ -0,0 +1,5 @@
+class Config:
+    BEBUG = True
+    SECRET_KEY = 'QIANG1'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/qiang1'
+    SQLALCHEMY_TRACK_MODIFICATTONS = True
